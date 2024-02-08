fd2b78a0035bf1be41cee4234c2246e7ef5c61e0
WounderCreeeen <artemlukanin65@gmail.com>
2024-02-08 07:42:42 +0300

    change log: 1. some bugs fixed 2. added new bugs to fix in next patch

--- db_scripts.py
+++ db_scripts.py
@@ -1,5 +1,5 @@
-# Здесь будет код веб-приложения
 import sqlite3
+from random import randint
 
 bd_name = 'quiz.sqlite'
 conn = None
@@ -14,12 +14,50 @@ def open_db():
 
 def close_db():
     cursor.close()
-    conn.commit()
+    conn.close()
 
 
 def do(query):
     cursor.execute(query)
     conn.commit()
+
+
+def clear_db():
+    open_db()
+    query = '''DROP TABLE IF EXISTS quiz_content'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS question'''
+    do(query)
+    query = '''DROP TABLE IF EXISTS quiz'''
+    do(query)
+    close_db()
+
+
+def create_db():
+    open_db()
+    cursor.execute('''PRAGMA foreign_keys=on''')
+
+    do('''CREATE TABLE IF NOT EXISTS quiz (
+            id INTEGER PRIMARY KEY, 
+            name VARCHAR)'''
+       )
+    do('''CREATE TABLE IF NOT EXISTS question (
+                id INTEGER PRIMARY KEY, 
+                question VARCHAR, 
+                answer VARCHAR, 
+                wrong1 VARCHAR, 
+                wrong2 VARCHAR, 
+                wrong3 VARCHAR)'''
+       )
+    do('''CREATE TABLE IF NOT EXISTS quiz_content (
+                id INTEGER PRIMARY KEY,
+                quiz_id INTEGER,
+                question_id INTEGER,
+                FOREIGN KEY (quiz_id) REFERENCES quiz (id),
+                FOREIGN KEY (question_id) REFERENCES question (id) )'''
+       )
+    close_db()
+
 
 def show(table):
     query = 'SELECT * FROM ' + table
@@ -28,95 +66,69 @@ def show(table):
     print(cursor.fetchall())
     close_db()
 
-def create_db():
-    open_db()
-    cursor.execute('PRAGMA foreign_keys=on')
-    do("""CREATE TABLE IF NOT EXISTS quiz (
-            quiz_id INTAGER PRIMARY KEY, name VARCHAR)""")
-
-    do("""CREATE TABLE IF NOT EXISTS question (
-        question_id INTAGER PRIMARY KEY,
-            question VARCHAR,
-            answer VARCHAR,
-            wrong1 VARCHAR,
-            wrong2 VARCHAR,
-            wrong3 VARCHAR)""")
-
-    do("""CREATE TABLE IF NOT EXISTS quiz_content (
-        id INTAGER PRIMARY KEY,
-        question_id INTAGER,
-        FOREIGN KEY (quiz_id) REFERENCES quiz (id),
-        FOREIGN KEY (question_id) REFERENCES question (id))""")
-    close_db()
-
-
-def clear_db():
-    open_db()
-    query = """DROP TABLE IF EXISTS quiz_content"""
-    do(query)
-    query = """DROP TABLE IF EXISTS question"""
-    do(query)
-    query = """DROP TABLE IF EXISTS quiz"""
-    do(query)
-    close_db()
-
-def add_quiz():
-    quizes = [
-        ('Своя игра'),
-        ('Кто хочет стать миллионером'),
-        ('Самый умный')
-    ]
-    open_db()
-    cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''')
-
-def add_questions():
-    questions = [
-        ('q1','ra1','wa11','wa12','wa13'),
-        ('q2','ra2','wa21','wa22','wa23'),
-        ('q3','ra3','wa31','wa32','wa33'),
-        ('q4','ra4','wa41','wa42','wa43'),
-        ('q5','ra5','wa51','wa52','wa53'),
-        ('q6','ra6','wa61','wa62','wa63'),
-        ('q7','ra7','wa71','wa72','wa73')
-    ]
-    open_db()
-    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?, ?, ?, ?, ?)''',
-                       questions
-    )
-    conn.commit()
-    close_db()
 
 def show_tables():
     show('question')
     show('quiz')
     show('quiz_content')
 
+
+def add_questions():
+    questions = [
+        ('q1', 'ra1', 'wa11', 'wa12', 'wa13'),
+        ('q2', 'ra2', 'wa21', 'wa22', 'wa23'),
+        ('q3', 'ra3', 'wa31', 'wa32', 'wa33'),
+        ('q4', 'ra4', 'wa41', 'wa42', 'wa43'),
+        ('q5', 'ra5', 'wa51', 'wa52', 'wa53'),
+        ('q6', 'ra6', 'wa61', 'wa62', 'wa63'),
+        ('q7', 'ra7', 'wa71', 'wa72', 'wa73')
+    ]
+    open_db()
+    cursor.executemany('''INSERT INTO question (question, answer, wrong1, wrong2, wrong3) VALUES (?,?,?,?,?)''',
+                       questions)
+    conn.commit()
+    close_db()
+
+
+def add_quiz():
+    quizes = [
+        ('q1',),
+        ('q2',),
+        ('q3',)
+    ]
+    open_db()
+    cursor.executemany('''INSERT INTO quiz (name) VALUES (?)''', quizes)
+    conn.commit()
+    close_db()
+
+
 def add_links():
     open_db()
     cursor.execute('''PRAGMA foreign_keys=on''')
-    query='INSERT INTO quiz_content (quiz_id, question_id) VALUES (?, ?)'
-    answer=input('Добавить связь (y/n)?')
+    query = "INSERT INTO quiz_content (quiz_id, question_id) VALUES (?,?)"
+    answer = input("Добавить связь (y / n)?")
     while answer != 'n':
-        quiz_id = int(input('id викторины'))
-        question_id = int(input('id вопроса'))
+        quiz_id = int(input("id викторины: "))
+        question_id = int(input("id вопроса: "))
         cursor.execute(query, [quiz_id, question_id])
         conn.commit()
-        answer = input('Добавить связь (y/n)?')
+        answer = input("Добавить связь (y / n)?")
     close_db()
 
-def get_question_after(question_id=0,quiz_id=1):
+
+def get_question_after(last_id=0, vict_id=1):
     open_db()
-    query='''
-    SELECT quiz_content.id, question.question, question.answer,
-    question.wrong1, question.wrong2, question.wrong3
-    FROM question, quiz_content
-    WHERE quiz_content.id > ? AND quiz_content.quiz_id == ?
-    ORDER BY quiz_content.id
-    '''
-    cursor.execute(query, [question_id, quiz_id])
+    query = '''
+        SELECT quiz_content.id, question.question, question.answer, question.wrong1, question.wrong2, question.wrong3
+        FROM question, quiz_content 
+        WHERE quiz_content.question_id == question.id
+        AND quiz_content.id > ? AND quiz_content.quiz_id == ? 
+        ORDER BY quiz_content.id '''
+    cursor.execute(query, [last_id, vict_id])
     result = cursor.fetchone()
     close_db()
     return result
+
 
 def get_quizes():
     query = 'SELECT = FROM quiz ORDER BY id'
@@ -126,14 +138,58 @@ def get_quizes():
     close_db()
     return result
 
+
+def get_quiz_count():
+    query = 'SELECT MAX(quiz_id) FROM quiz_content'
+    open_db()
+    cursor.execute(query)
+    result = cursor.fetchone()
+    close_db()
+    return result
+
+
+def get_random_quiz_id():
+    query = 'SELECT quiz_id FROM quiz_content'
+    open_db()
+    cursor.execute(query)
+    ids = cursor.fetchall()
+    rand_num = randint(0, len(ids) - 1)
+    rand_id = ids[rand_num][0]
+    close_db()
+    return rand_id
+
+
+def check_answer(q_id, ans_text):
+    query = '''
+    SELECT question.answer
+    FORM quiz_content, question
+    WHERE quiz_content.id = ?
+    AND quiz_content.question_id = question_id
+    '''
+    open_db()
+    cursor.execute(query, str(q_id))
+    result = cursor.fetchone()
+    close_db()
+    if result is None:
+        return False
+    else:
+        if result[0] == ans_text:
+            return True
+        return False
+
+
 def main():
     clear_db()
     create_db()
     add_questions()
     add_quiz()
+    show_tables()
     add_links()
     show_tables()
-    #print(get_question_after(3,1))
+    # print(get_question_after(0, 3))
+    # print(get_quiz_count())
+    # print(get_random_quiz_id())
+
 
 if __name__ == '__main__':
     main()
--- quiz.py
+++ quiz.py
@@ -1,53 +1,89 @@
+import os
 from flask import *
 from db_scripts import *
-from random import randint
+from random import randint, shuffle
+
 
 def start_quiz(quiz_id):
     session['quiz'] = quiz_id
     session['last_question'] = 0
+    session['answers'] = 0
+    session['total'] = 0
+
 
 def end_quiz():
     session.clear()
 
-def quiz_form():
-    text1 = '''
-    <html><body><h2>Выберите викторину: <h2><form method="post" action="index"><select name="quiz">
-    '''
-    text2 = '''
-    <p><input type="submit" value="Выбрать"></p>
-    '''
-    text3='''</select'''
 
+def quiz_form():
+    text1 = '''<html><body><h2>Выберите викторину: <h2><form method="post" action="index"><select name="quiz">'''
+    text2 = '''<p><input type="submit" value="Выбрать"></p>'''
+    text3 = '''</select'''
     text4 = '''</form></body></html>'''
+    options = ''' '''
+    q_list = get_quizes()
+    for id, name in q_list:
+        text = ('''<option value="''' + str(id) + '''">''' + str(name) + '''</option>''')
+        options = options + text
+    return text1 + options + text3
+
+
 def index():
     if request.method == 'GET':
         start_quiz(-1)
         return quiz_form()
     else:
-        end_quiz()
+        pass
 
     max_quiz = 3
     session['quiz'] = randint(1, max_quiz)
     session['last_question'] = 0
     return '<a href="/test">Тест</a>'
+
+
+def save_answers():
+    answer = request.form.get('ans_text')
+    question_id = request.form.get('q_id')
+    session['last_question'] = question_id
+    session['total'] += 1
+    if check_answer(question_id, answer):
+        session['answers'] += 1
+
+
+def question_form(question):
+    answers_list = [
+        question[2], question[3], question[4], question[5]
+    ]
+    shuffle(answers_list)
+    return render_template('test.html', question=question[1], quest_id=question[0], answers_list=answers_list)
+
+
 def test():
-    result = get_question_after(session['last_question'], session['quiz'])
-    if result is None or len(result) == 0:
-        return redirect(url_for('result'))
+    if not ('quiz' in session) or int(session['quiz']) < 0:
+        return redirect(url_for('index'))
     else:
-        session['last_question'] = result[0]
-        string = '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
-        return string
+        if request.method == 'POST':
+            save_answers()
+        next_question = get_question_after(session['last_question'], session['quiz'])
+        if next_question is None or len(next_question) == 0:
+            # session['last_question'] = result[0]
+            return redirect(url_for('result'))
+        return question_form(next_question)
+
+
 def result():
-    return 'это все вопросы'
+    html = render_template('result.html', right=session['answers'], total=session['total'])
+    end_quiz()
+    return html
 
 
-app = Flask(__name__)
-app.app_url_rule('/', 'index', index)
-app.app_url_rule('/', 'test', test)
-app.app_url_rule('/', 'result', result)
+folder = os.getcwd()
+app = Flask(__name__, template_folder=folder, static_folder=folder)
+app.add_url_rule('/index', 'index', index, methods=['post', 'get'])
+app.add_url_rule('/test', 'test', test, methods=['post', 'get'])
+app.add_url_rule('/result', 'result', result)
 
-app.config['SECRET_KEY'] = 'Mayonnaise on an escalator'
+app.config['SECRET_KEY'] = 'MayonnaiseOnAnEscalator'
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run()
